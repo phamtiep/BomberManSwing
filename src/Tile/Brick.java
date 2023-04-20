2b63ad1f6b6ddb55c7fe036cda07f9e164fb11ef
HwuuPhuc0904 <lhuuphuc2004@gmail.com>
2023-04-20 21:25:10 +0700

    add tile

---
+++ src/Tile/Brick.java
@@ -0,0 +1,73 @@
+package Tile;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import Enitity.Entity;
+import Graphics.Sprite;
+
+public class Brick extends Entity {
+	
+	private static boolean initialized = false; 
+	private static BufferedImage brick;
+	private static BufferedImage[] brickExplodes; 
+	private boolean destroyed = false; 
+	private int aniTick , aniIndex , aniSpeed = 15;
+	
+	public Brick(int x, int y) {
+		super(x , y); 
+		brickExplodes = new BufferedImage[4];  
+		initBrick(); 
+		loadAnimationExplodes(); 
+	}
+	
+	
+
+	private void loadAnimationExplodes() {
+		for(int i = 0; i < 4; i++) {
+			brickExplodes[i] = sprite.getSprite(3, i); 
+		}
+	}
+
+
+
+	private void initBrick() {
+		sprite = new Sprite("/anhgame.png"); 
+		brick = sprite.getSprite(3, 0); 
+	}
+	
+	public void updateAnimation() {
+		aniTick++;
+		if(aniTick >= aniSpeed) {
+			aniTick = 0;
+			aniIndex++; 
+			if(aniIndex >= 4) {
+				aniIndex = 0;
+			}
+		}
+	}
+
+	public void ExploderBrick(Graphics g) {
+		updateAnimation();
+		g.drawImage(brickExplodes[aniIndex],2*32, 0*32, null); 
+	}
+
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.drawImage(brick, 1*32 , 0 * 32 , null);
+		ExploderBrick(g);
+	}
+	
+	@Override
+	public BufferedImage getImg() {
+		return this.brick; 
+	}
+
+}
---
+++ src/Tile/Grass.java
@@ -0,0 +1,41 @@
+package Tile;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import Enitity.Entity;
+import Graphics.Sprite;
+
+public class Grass extends Entity {
+	private static boolean initialized = false; 
+	private BufferedImage grass; 
+	
+	public Grass(int x , int y) {
+		super(x, y); 
+		initGrass(); 
+		initialized = true; 
+	}
+	
+
+	private void initGrass() {
+		sprite = new Sprite("/anhgame.png"); 
+		grass = sprite.getSprite(3, 4); 
+	}
+
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.drawImage(grass , 3 * 32 , 0, null); 
+	}
+	
+	@Override
+	public BufferedImage getImg() {
+		return this.grass; 
+	}
+}
